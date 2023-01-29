b11df593612be425709940eca135ed5bbd166ac0
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-01-29 22:57:21 +0530

    Add: Store the results in a dictionary for every computation

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -4,6 +4,29 @@ echo "Welcome to Sorting Arithmetic Computation Problem"
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
-echo "The three numbers are: $a $b $c"
-value=`awk 'BEGIN{printf("%0.2f",'$a%$b+$c')}'`;
-echo "$a % $b + $c = $value"
+echo "The three numbers are: $a, $b and $c"
+
+firstCompute=$(( $a + $b * $c ))
+echo "$a + $b * $c = $firstCompute"
+
+secondCompute=$(( $a * $b + $c ))
+echo "$a * $b + $c = $secondCompute"
+
+thirdCompute=`awk 'BEGIN{printf("%0.2f",'$c+$a/$b')}'`;
+echo "$c + $a / $b = $thirdCompute"
+
+fourthCompute=$(( $a % $b + $c ))
+echo "$a % $b + $c = $fourthCompute"
+
+declare -A dictionary
+
+
+dictionary[firstCompute]=$firstCompute
+dictionary[secondCompute]=$secondCompute
+dictionary[thirdCompute]=$thirdCompute
+dictionary[fourthCompute]=$fourthCompute
+
+for computation in ${!dictionary[@]}
+do
+	echo "dictionary[$computation] = ${dictionary[$computation]}"
+done
